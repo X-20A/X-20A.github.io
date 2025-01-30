da3d538aa86be162b44709fab0a26bd11fe9a33c
X-20A <155217226+X-20A@users.noreply.github.com>
2025-01-30 18:00:32 +0900

    修正: 白雪改二

--- data/ship.js
+++ data/ship.js
@@ -833,5 +833,5 @@ const s_data = [
 ,{"id":694,"name":"霧島改二丙","type":"巡洋戦艦","seek":16,"max_seek":52,"hp":85,"hp2":93,"ass":0,"max_ass":0,"fuel":100,"na":0,"sg":2}
 ,{"id":956,"name":"早霜改二","type":"駆逐艦","seek":11,"max_seek":42,"hp":33,"hp2":38,"ass":28,"max_ass":75,"fuel":15,"na":0,"sg":2}
 ,{"id":981,"name":"藤波改二","type":"駆逐艦","seek":12,"max_seek":56,"hp":33,"hp2":38,"ass":30,"max_ass":78,"fuel":15,"na":0,"sg":2}
-,{"id":986,"name":"白雪改二","type":"駆逐","seek":13,"max_seek":54,"hp":31,"hp2": 38,"asw": 26,"max_asw":65,"fuel":15,"na":0,"sg":2}
+,{"id":986,"name":"白雪改二","type":"駆逐艦","seek":13,"max_seek":54,"hp":31,"hp2": 38,"asw": 26,"max_asw":65,"fuel":15,"na":0,"sg":2}
 ];
