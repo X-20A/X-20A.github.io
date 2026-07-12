cc325cadf12d07e1163efc73b0de19f24d9f8759
X-20A <155217226+X-20A@users.noreply.github.com>
2026-07-12 11:28:40 +0900

    fix: 62-1

--- src/core/branch/world62/62-1.ts
+++ src/core/branch/world62/62-1.ts
@@ -121,13 +121,13 @@ export const calc_62_1: CalcFnWithCondition = (
             if (DD >= 6) {
                 return 'O2';
             }
-            if (CLE >= 1 && DD === 5) {
+            if (CLE === 1 && DD === 5) {
                 return 'O2';
             }
-            if (CLE >= 1 && DD === 4 && ships_length === 7 && is_fleet_speed_fast_or_more(speed)) {
+            if (CLE === 1 && DD === 4 && ships_length === 7 && is_fleet_speed_fast_or_more(speed)) {
                 return 'O2';
             }
-            if (CLE >= 1 && DD === 4 && ships_length === 7 && AV + LHA >= 1) {
+            if (CLE === 1 && DD === 4 && ships_length === 7 && AV + LHA >= 1) {
                 return 'O2';
             }
             return 'O1';
