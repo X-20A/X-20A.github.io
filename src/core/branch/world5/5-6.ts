b10562d8c9d1deda1fcbb9ad09b9b4558a7db1cb
X-20A <155217226+X-20A@users.noreply.github.com>
2026-07-17 03:16:07 +0900

    fix: 5-6-B

--- src/core/branch/world5/5-6.ts
+++ src/core/branch/world5/5-6.ts
@@ -81,26 +81,17 @@ export const calc_5_6: CalcFnWithCondition = (
             if (BBV === 2 && Ds === 4) {
                 return 'C2';
             }
-            if (Ss >= 1) {
-                return  'C1';
+            if (BBs >= 2) {
+                return 'C1';
             }
             if (CVH >= 1) {
                 return 'C1';
             }
-            if (ships_length <= 5) {
-                return 'C';
-            }
-            if (BBs + CVL >= 2) {
-                return 'C1';
-            }
-            if (BBs + CVL === 0) {
-                return 'C';
-            }
             if (is_fleet_speed_fast_or_more(speed)) {
                 return 'C';
             }
-            if (Ds === 3) {
-                return 'C';
+            if (CAs + CLE + CLT + AV + LHA >= 2 && Ds >= 3) {
+                return 'C'
             }
             return 'C1';
         case 'C1':
